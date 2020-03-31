256e9abff05e9688fcb7890216df8a606ac3946d
jorgvillo <62618515+jorgvillo@users.noreply.github.com>
2020-03-31 13:33:15 +0200

    2

--- GitHub/src/github/GitHub.java
+++ GitHub/src/github/GitHub.java
@@ -3,5 +3,6 @@ package github;
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("Prueba Push");
+        System.out.println("Prueba Pull");
     }    
 }
